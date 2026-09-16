88993ceb5edb20af67fc95c28dab86dda044f7c1
kanon-4510 <zaq.yoshi.ikm@gmail.com>
2026-09-16 15:27:56 +0900

    ストーリーとバトルの画面切り替えをできる様にし③益田

--- Src/Scene/Phase/BattlePhase.cpp
+++ Src/Scene/Phase/BattlePhase.cpp
@@ -6,7 +6,9 @@
 BattlePhase::BattlePhase(GameScene& gameScene)
 	: gameScene_(gameScene)
 {
-
+	battleStep_ = BATTLE_STEP::COMMAND_SELECT;
+	commandSelect_ = COMMAND_SELECT::ATTACK;
+	
 }
 
 BattlePhase::~BattlePhase()
@@ -21,6 +23,8 @@ void BattlePhase::Update()
 {
 	auto& ins = InputManager::GetInstance();
 
+	TurnManager();
+
 	if (ins.IsTrgDown(KEY_INPUT_RETURN))
 	{
 		gameScene_.ChangePhase(GameScene::QUEST_PHASE::PHASE_STORY);
@@ -29,9 +33,70 @@ void BattlePhase::Update()
 
 void BattlePhase::Draw()
 {
-	DrawString(100, 100, "Battle Phase", GetColor(255, 0, 0));
+	DrawString(0, 0, "Battle Phase", GetColor(255, 0, 0));
+	DrawCommandMenu(0, 40, { "こうげき", "話す", "逃げる" }, static_cast<int>(commandSelect_));
 }
 
 void BattlePhase::Release()
 {
+}
+
+void BattlePhase::TurnManager(void)
+{
+	switch (battleStep_)
+	{
+	case BattlePhase::BATTLE_STEP::COMMAND_SELECT:
+		CommandSelect();
+		break;
+	case BattlePhase::BATTLE_STEP::PLAYER_ATTACK:
+		PlayerAttack();
+		break;
+	case BattlePhase::BATTLE_STEP::ENEMY_ATTACK:
+		EnemyAttack();
+		break;
+	case BattlePhase::BATTLE_STEP::MAX:
+		break;
+	default:
+		break;
+	}
+}
+
+void BattlePhase::CommandSelect(void)
+{
+	// コマンド選択の処理
+	int commandIndex = static_cast<int>(commandSelect_);
+	ProcessCommandMenuSelection(commandIndex, static_cast<int>(COMMAND_SELECT::MAX));
+	commandSelect_ = static_cast<COMMAND_SELECT>(commandIndex);
+}
+
+void BattlePhase::PlayerAttack(void)
+{
+}
+
+void BattlePhase::EnemyAttack(void)
+{
+}
+
+
+void BattlePhase::DrawCommandMenu(int x, int y, const std::vector<std::string>& labels, int cursor, int interval)
+{
+	for (int i = 0; i < (int)labels.size(); ++i) {
+		int color = GetColor(255, 255, 255);
+		int selectColor = GetColor(255, 255, 0); //選択中は黄色にする
+		DrawFormatString(x, y + i * interval, (cursor == i ? selectColor : color), labels[i].c_str());
+	}
+}
+
+void BattlePhase::ProcessCommandMenuSelection(int& cursor, int maxItems)
+{
+	if (InputManager::GetInstance().IsTrgDown(KEY_INPUT_UP) ||
+		InputManager::GetInstance().IsPadBtnTrgDown(InputManager::JOYPAD_NO::PAD1, InputManager::JOYPAD_BTN::DG_UP))
+	{
+		cursor = (cursor - 1 + maxItems) % maxItems; //上にループ
+	}
+	if (InputManager::GetInstance().IsTrgDown(KEY_INPUT_DOWN) ||
+		InputManager::GetInstance().IsPadBtnTrgDown(InputManager::JOYPAD_NO::PAD1, InputManager::JOYPAD_BTN::DG_DOWN))
+	{
+		cursor = (cursor + 1) % maxItems; //下にループ
+	}
 }
--- Src/Scene/Phase/BattlePhase.h
+++ Src/Scene/Phase/BattlePhase.h
@@ -6,6 +6,23 @@ class GameScene;
 class BattlePhase : public PhaseBase
 {
 public:
+
+	enum class BATTLE_STEP
+	{
+		COMMAND_SELECT,	//コマンド選択
+		PLAYER_ATTACK,	//プレイヤー攻撃
+		ENEMY_ATTACK,	//敵攻撃
+		MAX
+	};
+
+	enum class COMMAND_SELECT
+	{
+		ATTACK,	//攻撃
+		TALK,	//会話
+		RUN,	//逃走
+		MAX
+	};
+
 	BattlePhase(GameScene& gameScene);
 	~BattlePhase();
 	void Init(void)override;
@@ -15,4 +32,20 @@ public:
 private:
 
 	GameScene& gameScene_;			//親の情報を渡す
+
+	BATTLE_STEP battleStep_;		//バトルステップ
+	COMMAND_SELECT commandSelect_;	//コマンド選択
+
+	void TurnManager(void);		//ターン管理
+
+	void CommandSelect(void);	//コマンド選択
+	void PlayerAttack(void);	//プレイヤー攻撃
+	void EnemyAttack(void);		//敵攻撃
+
+	// 引数に vector を使うことで、3択でも4択でも対応可能にします
+	void DrawCommandMenu(int x, int y, const std::vector<std::string>& labels, int cursor, int interval = 40);
+
+	// DrawCommandMenuの数に合わせて選択をできるようにする
+	void ProcessCommandMenuSelection(int& cursor, int maxItems);
+
 };
